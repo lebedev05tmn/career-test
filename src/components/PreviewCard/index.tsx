08033a2ea0f33527226ff17a763ac05b99bae0d6
lebedev05tmn <lebedev05tmn@gmail.com>
2024-11-20 19:04:04 +0300

    theme fix

--- src/components/PreviewCard/index.tsx
+++ src/components/PreviewCard/index.tsx
@@ -1,18 +1,16 @@
 import type { FC } from 'react';
 import type { TPreviewCard } from './types';
-import { Avatar, Card, useAppearance } from '@vkontakte/vkui';
+import { Avatar, Card } from '@vkontakte/vkui';
 import Button from '../../ui/Button';
 
 const PreviewCard: FC<TPreviewCard> = ({ title, description, image, action, buttonTitle }) => {
-    const appearance = useAppearance();
-
     return (
         <Card className="w-full flex flex-col items-center pb-10">
             <h2 className="pt-5 pb-5">{title}</h2>
             <div className="flex items-center flex-col w-[calc(100%-26px)] rounded-[15px] bg-[#363738]">
                 <Avatar className="mt-[47px] mb-[42px]" size={96} src={image} />
                 <h2
-                    className={`w-[calc(100%-40px)] text-center whitespace-nowrap mb-20 ${appearance === 'light' ? 'text-white' : 'text-black'}`}
+                    className={'w-[calc(100%-40px)] text-center whitespace-nowrap mb-20 text-white'}
                     dangerouslySetInnerHTML={{ __html: description }}
                 />
                 <Button
